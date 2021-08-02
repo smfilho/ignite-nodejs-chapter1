e8ddd2d67535aab7ad3f6f40a7085e8d6496502e
Silvio Melo Filho <smfilho@gmail.com>
2021-08-02 10:45:35 -0500

    first committ

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/* 
+SSN - string
+name - string
+id - uuid
+statement - []
+*/
+
+// app.get('/', (request, response) => {
+//   return response.json('Esse é o início, próxima rota está em /courses');
+// });
+
+app.post('/account', (request, response) => {
+  const { ssn, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    customer => customer.ssn === ssn
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists' });
+  }
+
+  //const id = uuidv4();
+
+  customers.push({
+    ssn,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+// app.put('/courses/:id', (request, response) => {
+//   const { id } = request.params;
+//   console.log(id);
+//   return response.json(['Class999', 'Class2', 'Class3', 'Class4']);
+// });
+
+// app.patch('/courses/:id', (request, response) => {
+//   return response.json(['Class999', 'Class888', 'Class3', 'Class4']);
+// });
+
+// app.delete('/courses/:id', (request, response) => {
+//   return response.json(['Class999', 'Class888', 'Class4']);
+// });
+
+app.listen(3333);
