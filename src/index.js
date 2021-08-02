0a0eec5e256672830c9a8b467dbdc370d99c7b59
Silvio Melo Filho <smfilho@gmail.com>
2021-08-02 11:46:46 -0500

    asasdasdsa

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ id - uuid
 statement - []
 */
 
+// askhasdhaslkjdsal
+
 // app.get('/', (request, response) => {
 //   return response.json('Esse é o início, próxima rota está em /courses');
 // });
